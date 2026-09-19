ea8492f7ba3b58e67b4319a2af887da858665a94
hudrucan <huy.nguyen@employmenthero.com>
2026-09-19 22:38:23 +0700

    chore: remove unused management and digital-human apps

--- main/manager-api/src/main/resources/db/changelog/202608061030.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `ai_model_config`
-SET `config_json` = JSON_SET(`config_json`, '$.model_name', 'deepseek-v4-flash')
-WHERE `id` = 'LLM_DeepSeekLLM'
-  AND JSON_EXTRACT(`config_json`, '$.model_name') = 'deepseek-chat';
--- main/manager-api/src/main/resources/db/changelog/202608131030.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `sys_params`
-SET `param_value` = '15'
-WHERE `param_code` = 'tts_timeout'
-  AND `param_value` = '10';
